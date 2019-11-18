0b11f364de4dc0d28a7ac662b5cfc619214d350b
paullarachel <paullarachel12@hotmail.com>
2019-11-18 01:08:20 -0300

    iniciando models

--- src/nemli/gerenciador/models.py
+++ src/nemli/gerenciador/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Livro(models.Model):
+    nome = models.CharField(max_length=200)
+    isbn_13 = models.CharField(max_length=13)
+    sinopse = models.CharField(max_length=2000)
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=200)
+
+class AutorLivro(models.Model):
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT)
+    livro = models.ForeignKey(Livro, on_delete=models.PROTECT)
