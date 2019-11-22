ec9f0ecc48e47721d68c4ce6f70a3475bb9061c0
paullarachel <paullarachel12@hotmail.com>
2019-11-21 23:09:47 -0300

    iniciando views

--- src/nemli/gerenciador/models.py
+++ src/nemli/gerenciador/models.py
@@ -6,6 +6,7 @@ class Livro(models.Model):
     nome = models.CharField(max_length=200)
     isbn_13 = models.CharField(max_length=13)
     sinopse = models.CharField(max_length=2000)
+    ano = models.CharField(max_length=4)
 
 class Autor(models.Model):
     nome = models.CharField(max_length=200)
--- src/nemli/gerenciador/views.py
+++ src/nemli/gerenciador/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Livro, AutorLivro, Autor
 
 # Create your views here.
+
+def paginaBiblioteca(request):
+    livros = Livro.objects.all()
+    context = {'livros': livros}
+    return render(request, '', context)
+
+def adicionarLivro(request):
+    livro = Livro()
+    livro.nome = request.POST['nome']
+    livro.isbn_13 = request.POST['isbn_13']
+    livro.ano = request.POST['ano']
+    livro.sinopse = request.POST['sinopse']
+    livro.save()
